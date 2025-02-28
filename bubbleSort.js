ed7c43f36047166ed9c7d879eebb16d4cf0d4f32
jishad kolapurath <jishadkolapurath@gmail.com>
2025-02-28 20:00:24 +0530

    insertion sort implemented

--- bubbleSort.js
+++ bubbleSort.js
@@ -35,4 +35,54 @@ function optimizedBubbleSort(array) {
     return array;
 }
 
-console.log(optimizedBubbleSort([1,2,8,4,5,6]))
+console.log(optimizedBubbleSort([1, 2, 8, 4, 5, 6]));
+
+//modify the bubble sort to do descending order instead of ascending order;
+
+function descendingSort(array) {
+    let swapped = false;
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length - i - 1; j++) {
+            if (array[j] < array[j + 1]) {
+                swapped = true;
+                [array[j], array[j + 1]] = [array[j + 1], array[j]];
+            }
+        }
+
+        if (!swapped) break;
+    }
+    return array;
+}
+
+console.log(descendingSort([1, 2, 8, 4, 5, 6]));
+
+//count the number of swaps
+
+function countSwaps(array) {
+    let count = 0;
+    let swapped = false;
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = 0; j < array.length - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                count++;
+                swapped = true;
+                [array[j], array[j + 1]] = [array[j + 1], array[j]];
+            }
+        }
+        if (!swapped) break;
+    }
+    return count;
+}
+
+console.log(countSwaps([1, 2, 8, 4, 5, 6]));
+
+//recursive bubble sort
+
+function recursiveBubbleSort(array, index = 0) {
+    if (index == array.length - 1) return array;
+    if (array[index] > array[index + 1]) {
+        [array[index], array[index + 1]] = [array[index + 1], array[index]];
+    }
+
+    return ;
+}
---
+++ insertionSort.js
@@ -0,0 +1,17 @@
+//insertion sort implementation
+
+function insertionSort(array) {
+    for (let i = 1; i < array.length; i++) {
+        let key = array[i];
+        let j = i - 1;
+        while (j >= 0 && key < array[j]) {
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j + 1] = key;
+    }
+
+    return array;
+}
+//example
+console.log(insertionSort([5, 3]));
